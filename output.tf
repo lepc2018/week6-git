be465ee11996f1f43b8f342b87eed28941327ce5
lepc2018 <lecp200@gmail.com>
2024-01-30 21:18:34 -0500

    lepc enter

--- output.tf
+++ output.tf
@@ -1,6 +1,7 @@
-output "instance_ip" {
-  value = aws_lightsail_instance.custom.public_ip_address
+output "pip" {
+    value = aws_lightsail_instance.server1.public_ip_address
 }
-output "my-arn" {
-  value = aws_lightsail_instance.custom.arn
-}  
+
+output "privip" {
+    value = aws_lightsail_instance.server1.private_ip_address
+}
